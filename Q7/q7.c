36c2ac1eb0fe33d9ee6039fd5cb6007b2dc9843f
Ioan Troana <ioantroana@Ioans-MacBook-Pro.local>
2019-05-03 16:52:45 +0100

    finished q8

--- Q7/q7.c
+++ Q7/q7.c
@@ -11,37 +11,6 @@ redacted text is saved in an output file*/
 char redact_words[10][30];
 FILE *outfile;
 
-// // read redacted words
-// void getRedactWords(char *red_file) {
-//   FILE *file = fopen(red_file, "r");
-//   char *ptok = NULL;
-
-//   if (file != NULL) {
-//     char line[30];
-//     int i = 0;
-
-//     while (fgets(line, 30, file) != NULL) /* read a line */
-//     {
-//       int len = strlen(line);
-//       if (line[len - 1] == '\n') line[len - 1] = '\0';
-
-//       ptok = strtok(line, " ");
-//       while (ptok != NULL) {
-//         strcpy(redact_words[i], ptok);
-
-//         ptok = strtok(NULL, " ");
-//         i++;
-//       }
-//     }
-//     fclose(file);
-//   } else {
-//     perror(red_file);
-//   }
-//   printf("Words to replace:\n");
-//   int arr_size = sizeof(redact_words) / sizeof(redact_words[0]);
-//   for (int i = 0; i < arr_size; i++) printf("%s\n", redact_words[i]);
-// }
-
 // reads a file into a char buffer
 char *read_file(char *path, int *length) {
   FILE *fp = fopen(path, "r");
@@ -133,25 +102,9 @@ void redact_word(char *word) {
   }
 }
 
-int equal(char *w1, char *w2) {
-  return strcasecmp(w1, w2);
-
-  // int i = 0;
-  // while (tolower(w1[i]) == tolower(w2[i]) && w1[i] != '\0' && w2[i] != '\0')
-  // {
-  //   i++;
-  // }
-
-  // if (w1[i] != '\0' && w2[i] != '\0') {
-  //   return 1;
-  // }
-
-  // return 0;
-}
-
 int is_redactable(char *word, char *redactable[], int redactable_count) {
   for (int i = 0; i < redactable_count; i++) {
-    if (equal(word, redactable[i]) == 0) {
+    if (strcasecmp(word, redactable[i]) == 0) {
       return 1;
     }
   }
@@ -181,57 +134,6 @@ void redact_text(char *text, int text_length, char *redact_words,
 
   *output_length = tokenized_text_length;
 }
-
-// // checks if word is redactable
-// int isRedactable(char *word, char *redact_words[]) {
-//   int length = (int)(sizeof(redact_words) / sizeof(redact_words[0]));
-
-//   for (int i = 0; i < length; i++) {
-//     if (strcasecmp(word, redact_words[i]) == 0) {
-//       return strlen(word);
-//     }
-//   }
-//   return 0;
-// }
-
-// char *asterisk_str(int n) {
-//   char *x = malloc(sizeof(char) * (n + 1));
-//   for (int i = 0; i < n; i++) {
-//     x[i] = '*';
-//   }
-//   return x;
-// }
-
-// // redact
-// char *redact(char *redact_words[], char *text) {
-//   char *redactedText = malloc(sizeof(text));
-//   char *text_words[1000];
-//   int text_words_count;
-//   splitWords(text, " ", text_words, &text_words_count);
-
-//   for (int i = 0; i < text_words_count - 1; i++) {
-//     int n = isRedactable(text_words[i], redact_words);
-//     if (n > 0) {
-//       char *x = asterisk_str(n);
-//       strcat(redactedText, x);
-//       free(x);
-//     } else {
-//       strcat(redactedText, text_words[i]);
-//     }
-//     strcat(redactedText, " ");
-//   }
-
-//   int n = isRedactable(text_words[text_words_count - 1], redact_words);
-//   if (n > 0) {
-//     char *x = asterisk_str(n);
-//     strcat(redactedText, x);
-//     free(x);
-//   } else {
-//     strcat(redactedText, text_words[text_words_count - 1]);
-//   }
-
-//   return redactedText;
-// }
 
 int main(int argc, char *argv[]) {
   char *input_redacted_words_path;
@@ -268,124 +170,3 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
-
-// int main(int argc, char *argv[]) {
-//   char ch;
-//   FILE *fp;
-//   char cur_path[] = "";
-//   char r_txt_file[100] = "debate1.txt";
-//   char r_redact_file[100] = "redact1.txt";
-//   char w_redacted_txt[100] = "redacted_out.txt";
-//   char line[500];
-//   char replaced_line[500] = "\0";
-//   char extra_char[2] = "\0";
-//   char *token;
-//   char temp_tok[30];
-//   int appendNewline = 0;
-
-//   // argv 1 - name of file to read in text
-//   // argv 2 - name of file to read in list of words to redact
-//   // argv 3 - name of file to write out the redacted text
-//   if (argc == 3) {
-//     printf("%s\n", argv[1]);
-//     printf("%s\n", argv[2]);
-//     strcpy(r_txt_file, cur_path);
-//     strcat(r_txt_file, argv[1]);
-//     strcpy(r_redact_file, cur_path);
-//     strcat(r_redact_file, argv[2]);
-//   } else if (argc == 4) {
-//     printf("%s\n", argv[1]);
-//     printf("%s\n", argv[2]);
-//     printf("%s\n", argv[3]);
-//     strcpy(r_txt_file, cur_path);
-//     strcat(r_txt_file, argv[1]);
-//     strcpy(r_redact_file, cur_path);
-//     strcat(r_redact_file, argv[2]);
-//     strcpy(w_redacted_txt, cur_path);
-//     strcat(w_redacted_txt, argv[3]);
-//   }
-
-//   getRedactWords(r_redact_file);
-
-//   printf("Input file is %s\n", r_txt_file);
-
-//   outfile = fopen(w_redacted_txt, "w+");
-
-//   fp = fopen(r_txt_file, "r");
-//   if (fp == NULL) {
-//     perror("Error while opening the file.\n");
-//     exit(-1);
-//   }
-
-//   printf("\nSentence to redact (substitute with *):\n");
-//   while (fgets(line, 500, fp) != NULL) {
-//     token = strtok(line, " ");
-
-//     int word_len = isRedactable(token);
-//     int x;
-//     char asterisk[30];
-//     if (word_len > 0) {
-//       for (x = 0; x < word_len; x++) asterisk[x] = '*';
-//       asterisk[x] = '\0';
-
-//       strcat(replaced_line, asterisk);
-//     } else {
-//       strcat(replaced_line, token);
-//     }
-//     strcat(replaced_line, " ");
-
-//     // get other tokens
-//     while (token != NULL) {
-//       token = strtok(NULL, " ");
-
-//       if (token != NULL) {
-//         // has word got extra , or .
-//         strcpy(temp_tok, token);
-//         int last_char = strlen(temp_tok) - 1;
-//         if (temp_tok[last_char] == '\n') {
-//           last_char--;
-//           appendNewline = 1;
-//         }
-
-//         if (isalpha(temp_tok[last_char]) == 0) {
-//           if (isdigit(temp_tok[last_char]) == 0) {
-//             extra_char[0] = temp_tok[last_char];
-//             temp_tok[last_char] = '\0';
-//           }
-//         }
-
-//         word_len = isRedactable(temp_tok);
-//         asterisk[word_len];
-
-//         if (word_len > 0) {
-//           for (x = 0; x < word_len; x++) asterisk[x] = '*';
-//           asterisk[x] = '\0';
-
-//           strcat(replaced_line, asterisk);
-//           if (extra_char[0] != '\0') {
-//             strcat(replaced_line, extra_char);
-//           }
-//           if (appendNewline == 1) {
-//             strcat(replaced_line, "\n");
-//           }
-//         } else {
-//           strcat(replaced_line, token);
-//         }
-//         strcat(replaced_line, " ");
-//         extra_char[0] = '\0';
-//         appendNewline = 0;
-//       } else {
-//         // remove last space
-//         replaced_line[strlen(replaced_line) - 1] = '\0';
-//       }
-//     }
-//     printf("%s", replaced_line);
-//     outputToFile(replaced_line);
-
-//     replaced_line[0] = '\0';
-//   }
-//   fclose(fp);
-//   fclose(outfile);
-
-//   return 0;
-// }
--- Q8/Q8Main.java
+++ Q8/Q8Main.java
@@ -1,32 +1,138 @@
 
+/* This program redacts redactable words from a given text file.
+A text file containing a pre-defined set of words to redact is also passed in as a program argument.
+ Once all the redactable words have been redacted, the new redacted text is saved in an output file*/
 
-public class Q8Main{
-	
-	public static void main(String[] args){
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Q8Main {
+
+	// private ArrayList<String> warAndPeace = new ArrayList<>();
+	// private ArrayList<String> redactWords = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
 		// args 0 - name of file to read in text
-		String inputFileName = null;
+		String inputFileName = "warandpeace.txt";
 		// args 1 - name of file to read in list of words to redact
-		String toRedact = null;
+		String toRedact = "redact.txt";
 		// args 2 - name of file to write out the redacted text
-		String outputFileName = null;
-		
-		if(args.length == 2){
+		String outputFileName = "redacted.txt";
+
+		if (args.length == 2) {
 			inputFileName = args[0];
 			toRedact = args[1];
-		}
-		else if(args.length == 3){
+		} else if (args.length == 3) {
 			inputFileName = args[0];
 			toRedact = args[1];
 			outputFileName = args[2];
 		}
-		
+
 		// call method to redact words from input text
-		new Q8Main().redactText(inputFileName,toRedact,outputFileName);
+		new Q8Main().redactText(inputFileName, toRedact, outputFileName);
 	}
-	
-	public void redactText(String input, String toRedact, String output){
-		System.out.println(input);
-		System.out.println(toRedact);
-		System.out.println(output);
+
+	public void redactText(String inputFileName, String toRedact, String outputFileName) throws IOException {
+
+		ArrayList<String> redactWords = readFile(toRedact);
+		ArrayList<String> warAndPeace = readFile(inputFileName);
+		redact(warAndPeace, redactWords);
+		System.out.println(redactWords);
+		saveRedactedText(outputFileName, warAndPeace);
+
 	}
+
+	// function to fill the length of the redacted word with '*'
+	private static String fill(int length) {
+		char[] chars = new char[length];
+		Arrays.fill(chars, '*');
+		return new String(chars);
+	}
+
+	// method to redact a string arraylist given an arraylist of words to redact
+	public void redact(ArrayList<String> text, ArrayList<String> redactWords) {
+
+		for (int i = 0; i < text.size(); i++) { // loop through array list of text
+			String textWord = text.get(i);
+			if (textWord.length() > 1) {
+				replaceRedactWords(textWord, text, redactWords, i); // replace all words in redactWords first
+				if (!redactWords.contains(textWord)) { // if textWord is a redactWord it should already have been
+																								// replaced
+					replaceNouns(textWord, text, redactWords, i); // then replace other redactable words aka nouns
+				}
+			}
+		}
+	}
+
+	// method to replace all redactWords that are in the text with stars
+	private void replaceRedactWords(String textWord, ArrayList<String> text, ArrayList<String> redactWords, int i) {
+		String redactWord = null;
+		for (int j = 0; j < redactWords.size(); j++) { // loop through array list of redactWords
+			redactWord = redactWords.get(j);
+
+			if (textWord.contains(redactWord)) {
+				String stars = textWord.replaceAll(redactWord, fill(redactWord.length()));
+				text.set(i, stars); // replace index i in text with new stars string
+				break; // no need to continue loop as textWord has been replaced in the text
+
+			}
+		}
+	}
+
+	// method to replace all redactible words in the text( that are not already in
+	// redactWords ) with stars
+	private void replaceNouns(String textWord, ArrayList<String> text, ArrayList<String> redactWords, int i) {
+
+		if (i != 0) { // not first word in text
+			if (Character.isUpperCase(textWord.charAt(0))) { // if first character of textWord is a Capital letter
+				String prevWord = text.get(i - 1); // get previous word
+				if (prevWord.length() > 1) {
+					char c = (prevWord.charAt(prevWord.length() - 1)); // get last character of previous word
+					if ((c != '.') && (c != '!') && (c != '?') && (c != '\'')) { // word is not after ./!/?/'
+						String stars = textWord.replaceAll("[A-Za-z]", "*"); // replace only alphabetical letters with
+																																	// stars
+						text.set(i, stars); // replace index i in text with new stars string
+					}
+				}
+			}
+		}
+	}
+
+	private ArrayList<String> readFile(String inputFileName) {
+		ClassLoader classLoader = getClass().getClassLoader(); // must load class
+		ArrayList<String> arrayList = new ArrayList<String>();
+
+		try (InputStream in = classLoader.getResourceAsStream(inputFileName);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				String[] words = line.split(" ");
+				arrayList.addAll(Arrays.asList(words));
+			}
+		} catch (IOException x) {
+			System.err.println(x);
+		}
+		System.out.println(arrayList);
+		return arrayList;
+	}
+
+	public void saveRedactedText(String outputFileName, ArrayList<String> warAndPeace) {
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(outputFileName, "UTF-8");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		StringBuilder builder = new StringBuilder();
+		for (String s : warAndPeace) {
+			builder.append(s + " ");
+		}
+		String str = builder.toString();
+		writer.println(str);
+		writer.close(); // must close writer
+	}
+
 }
