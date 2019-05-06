e511db44ddcc29598a069295d8448d7d4d073619
Ioan Troana <ioantroana@ioans-mbp-2.home>
2019-05-06 18:35:15 +0100

    q7 verified

--- Q7/q7.c
+++ Q7/q7.c
@@ -55,6 +55,8 @@ void write_file(char *path, char *words[], int count) {
   fclose(fp);
 }
 
+// tokenizes text into an array of words, with the option of saving the
+// separators as well
 void tokenize_text(char *text, int length, int save_separators, char *tokens[],
                    int *tokens_count) {
   int i = 0;
@@ -70,11 +72,12 @@ void tokenize_text(char *text, int length, int save_separators, char *tokens[],
   }
 
   while (ch != '\0') {
-    // if character or digit
+    // if character or digit add it to the buffer
     if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
         (ch >= '0' && ch <= '9')) {
       buffer[buffer_length++] = ch;
     } else {
+      // move the buffer contents into a token, as the word is complete
       for (int i = 0; i < buffer_length; i++) {
         tokens[tokens_length][i] = buffer[i];
       }
@@ -83,6 +86,7 @@ void tokenize_text(char *text, int length, int save_separators, char *tokens[],
       tokens_length++;
       buffer_length = 0;
 
+      // if specified, we can include the separators as tokens as well
       if (save_separators == 1) {
         tokens[tokens_length][0] = ch;
         tokens[tokens_length][1] = '\0';
@@ -95,6 +99,7 @@ void tokenize_text(char *text, int length, int save_separators, char *tokens[],
   *tokens_count = tokens_length;
 }
 
+// replaces every character in the word with *
 void redact_word(char *word) {
   int i = 0;
   while (word[i] != '\0') {
@@ -102,6 +107,7 @@ void redact_word(char *word) {
   }
 }
 
+// check if word is redactable or not
 int is_redactable(char *word, char *redactable[], int redactable_count) {
   for (int i = 0; i < redactable_count; i++) {
     if (strcasecmp(word, redactable[i]) == 0) {
@@ -111,6 +117,7 @@ int is_redactable(char *word, char *redactable[], int redactable_count) {
   return 0;
 }
 
+// tokenizes text and redacts the redactable words
 void redact_text(char *text, int text_length, char *redact_words,
                  int words_length, char *tokenized_text[], int *output_length) {
   // tokenize the input text
@@ -136,28 +143,29 @@ void redact_text(char *text, int text_length, char *redact_words,
 }
 
 int main(int argc, char *argv[]) {
-  char *input_redacted_words_path;
-  char *input_text_path;
-  char *output_text_path;
+  char *input_path = "debate.txt";
+  char *input_redact_path = "redact.txt";
+  char *output_path = "redacted_out.txt";
 
-  if (argc == 1) {
-    input_text_path = "debate1.txt";
-    input_redacted_words_path = "redact1.txt";
-    output_text_path = "redacted_out.txt";
-  } else if (argc == 3) {
-    input_text_path = argv[1];
-    input_redacted_words_path = argv[2];
-    output_text_path = "redacted_out.txt";
+  if (argc == 3) {
+    input_path = argv[1];
+    input_redact_path = argv[2];
   } else if (argc == 4) {
-    input_text_path = argv[1];
-    input_redacted_words_path = argv[2];
-    output_text_path = argv[3];
+    input_path = argv[1];
+    input_redact_path = argv[2];
+    output_path = argv[3];
   }
 
+  printf("Input text file: %s\n", input_path);
+  printf("Input redact file: %s\n", input_redact_path);
+  printf("Output file: %s\n", output_path);
+
+  // read the text and redactable words
   int text_length;
   int redact_words_text_length;
-  char *text = read_file("debate.txt", &text_length);
-  char *redact_words_text = read_file("redact.txt", &redact_words_text_length);
+  char *text = read_file(input_path, &text_length);
+  char *redact_words_text =
+      read_file(input_redact_path, &redact_words_text_length);
 
   char *redacted_txt_output[text_length];
   int redacted_txt_output_length;
@@ -165,8 +173,8 @@ int main(int argc, char *argv[]) {
   redact_text(text, text_length, redact_words_text, redact_words_text_length,
               redacted_txt_output, &redacted_txt_output_length);
 
-  write_file("redacted_out.txt", redacted_txt_output,
-             redacted_txt_output_length);
+  // write redacted words to a file
+  write_file(output_path, redacted_txt_output, redacted_txt_output_length);
 
   return 0;
 }
